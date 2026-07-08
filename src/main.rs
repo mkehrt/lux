826590ec50c9664e82b0004f0b211340c3bfbf39
Matthew Kehrt <mkehrt@roseandsigil.net>
2026-07-08 18:54:22 -0400

    Fake ctrl-C handling

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,21 @@
 use crossterm::event::{read, Event, KeyCode, KeyEvent, KeyEventKind, KeyEventState, KeyModifiers};
 use crossterm::terminal;
-use nix::unistd;
-use nix::sys::signal;
 use std::io::{self, Write as _};
+use std::process;
 
-fn main() -> io::Result<()>{
+fn main() -> io::Result<()> {
     let stdout = io::stdout();
     let mut locked_stdout = stdout.lock();
 
-    let raw_mode_was_enabled = terminal::is_raw_mode_enabled()?;
-    terminal::enable_raw_mode()?;
+    let mut previous_terminal_state = set_up_terminal()?;
+
     loop {
         let event = if let Ok(event) = read() {
             event
         } else {
             break;
         };
-        
+
         match event {
             Event::Key(KeyEvent {
                 code: KeyCode::Char('c'),
@@ -24,24 +23,42 @@ fn main() -> io::Result<()>{
                 kind: KeyEventKind::Press,
                 state: KeyEventState::NONE,
             }) => {
-                interrupt_self()?;
+                // In the future, this should send a SIGINT to self and do
+                // cleanup in the handler.  This means an external signal will
+                // be also do cleanup.
+                previous_terminal_state.restore()?;
+                process::exit(130);
             }
-            event => {
-                write!(locked_stdout, "{:?}", event)?;
+            _ => {
+                // \r Neede to move cursor to the beginning of the line.
+                write!(locked_stdout, "{:?}\r\n", event)?;
                 locked_stdout.flush()?;
             }
         }
     }
-
-    if raw_mode_was_enabled {
-        terminal::disable_raw_mode()?;
-    }
-
     Ok(())
 }
 
-fn interrupt_self() -> io::Result<()> {
-    let pid = unistd::getpid();
-    signal::kill(pid, signal::Signal::SIGINT)?;
-    Ok(())
+struct PreviousTerminalState {
+    raw_mode_was_enabled: bool,
+}
+
+impl PreviousTerminalState {
+    fn restore(&mut self) -> io::Result<()> {
+        if self.raw_mode_was_enabled {
+            terminal::enable_raw_mode()?;
+        } else {
+            terminal::disable_raw_mode()?;
+        }
+        println!("Restored terminal state");
+        Ok(())
+    }
+}
+
+fn set_up_terminal() -> io::Result<PreviousTerminalState> {
+    let raw_mode_was_enabled = terminal::is_raw_mode_enabled()?;
+    terminal::enable_raw_mode()?;
+    Ok(PreviousTerminalState {
+        raw_mode_was_enabled,
+    })
 }
