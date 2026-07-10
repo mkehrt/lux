ca8d549e81e95b569329d01ac12066bd18d13b84
Matthew Kehrt <mkehrt@roseandsigil.net>
2026-07-10 19:18:34 -0400

    More stuff

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
-use crossterm::event::{read, Event, KeyCode, KeyEvent, KeyEventKind, KeyEventState, KeyModifiers};
+use crossterm::event::{read, Event, KeyCode, KeyEvent, KeyEventKind, KeyModifiers};
+use crossterm::cursor;
+use crossterm::execute;
 use crossterm::terminal;
 use std::io::{self, Write as _};
 use std::process;
 
 fn main() -> io::Result<()> {
-    let stdout = io::stdout();
-    let mut locked_stdout = stdout.lock();
-
     let mut previous_terminal_state = set_up_terminal()?;
 
+    let mut terminal_size = get_terminal_size()?;
+
+    execute!(
+        io::stdout(),
+        terminal::Clear(terminal::ClearType::All))?;
+    
     loop {
         let event = if let Ok(event) = read() {
             event
@@ -21,7 +26,7 @@ fn main() -> io::Result<()> {
                 code: KeyCode::Char('c'),
                 modifiers: KeyModifiers::CONTROL,
                 kind: KeyEventKind::Press,
-                state: KeyEventState::NONE,
+                state: _,
             }) => {
                 // In the future, this should send a SIGINT to self and do
                 // cleanup in the handler.  This means an external signal will
@@ -29,22 +34,61 @@ fn main() -> io::Result<()> {
                 previous_terminal_state.restore()?;
                 process::exit(130);
             }
-            _ => {
-                // \r Neede to move cursor to the beginning of the line.
-                write!(locked_stdout, "{:?}\r\n", event)?;
-                locked_stdout.flush()?;
+            Event::Key(KeyEvent {
+                code: KeyCode::Char(c),
+                modifiers: KeyModifiers::NONE | KeyModifiers::SHIFT,
+                kind: KeyEventKind::Press,
+                state: _,
+            }) => {
+                let mut stdout = io::stdout();
+                write!(stdout, "{}", c)?;
+                stdout.flush()?;
+            }
+            Event::Resize(cols, rows) => {
+                terminal_size = TerminalSize { cols, rows };
+            }
+            other_event => {
+                handle_unknown_event(other_event, terminal_size.cols)?;
             }
         }
     }
     Ok(())
 }
 
+fn handle_unknown_event(event: Event, terminal_columns: u16) -> io::Result<()> {
+    let mut stdout = io::stdout();
+
+    execute!(
+        stdout,
+        cursor::SavePosition,
+        cursor::MoveTo(0, terminal_columns - 1),
+        terminal::Clear(terminal::ClearType::CurrentLine),
+    )?;
+    write!(stdout, "{:?}", event)?;
+    stdout.flush()?;
+
+    execute!(stdout, cursor::RestorePosition    )?;
+
+    Ok(())
+}
+
+#[derive(Debug)]
+struct TerminalSize {
+    rows: u16,
+    cols: u16,
+}
+
+fn get_terminal_size() -> io::Result<TerminalSize> {
+    let (cols, rows) = terminal::size()?;
+    Ok(TerminalSize { cols, rows })
+}
 struct PreviousTerminalState {
     raw_mode_was_enabled: bool,
 }
 
 impl PreviousTerminalState {
     fn restore(&mut self) -> io::Result<()> {
+        execute!(io::stdout(), terminal::LeaveAlternateScreen)?;
         if self.raw_mode_was_enabled {
             terminal::enable_raw_mode()?;
         } else {
@@ -58,6 +102,7 @@ impl PreviousTerminalState {
 fn set_up_terminal() -> io::Result<PreviousTerminalState> {
     let raw_mode_was_enabled = terminal::is_raw_mode_enabled()?;
     terminal::enable_raw_mode()?;
+    execute!(io::stdout(), terminal::EnterAlternateScreen)?;
     Ok(PreviousTerminalState {
         raw_mode_was_enabled,
     })
